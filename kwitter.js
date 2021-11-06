938276980853e0b660b31c08e50fe172bba6e55a
Veerenraana <88445519+Veerenraana@users.noreply.github.com>
2021-11-06 11:10:05 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,8 @@
+function adduser()
+
+{
+      username = document.getElementById("User_name").value;
+      localStorage.setItem("User_name",  username);
+      window.location = "kwitter_room.html";
+
+}
